43eeda3167247a9b3cb3de275ff5770ab1bd228f
bitwardenmagnus <meemsaas083@gmail.com>
2025-08-04 23:20:23 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const map = new maptilersdk.Map({
-  container: 'map',
-  style: maptilersdk.MapStyle.STREETS,
-  center: [0, 0],
-  zoom: 2,
-  attributionControl: false // <<< entfernt die Anzeige unten rechts
-});
